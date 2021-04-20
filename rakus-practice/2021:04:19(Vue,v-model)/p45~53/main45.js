669967b1c34b857c725cd868c621c60fa1ce30f9
cotton11aq <ka34a6hs5lvg@yahoo.co.jp>
2021-04-20 10:46:32 +0900

    rakus

--- rakus-practice/2021:04:19(Vue,v-model)/p45~53/main45.js
+++ rakus-practice/2021:04:19(Vue,v-model)/p45~53/main45.js
@@ -1,21 +1,46 @@
+const UserDetailChild = {
+  props: {
+    // joho: { type: Object },
+    userId: { type: Number },
+    userName: { type: String },
+    userAddress: { type: String },
+  },
+
+  template: `
+  <div>
+    <p>UserDetailの子供です</p>
+    {{ userId }}
+    {{ userName }}
+    {{ userAddress }}
+  </div>
+  `,
+};
+
+// 3
 const UserDetail = {
-  // 3
+  components: {
+    'user-detail-child': UserDetailChild,
+  },
+
   props: {
     user: { type: Object },
+    userId: { type: Number },
     userName: { type: String },
+    userAddress: { type: String },
   },
   template: `
   <div>
   <h2>選択中のユーザー</h2>
   <input v-model="user.name">
+  <user-detail-child :user-id = 'userId' :user-name = 'userName' :user-address = 'userAddress'></user-detail-child>
   </div>`,
 };
 
+// 2
 const ListTitle = {
-  // 2
   template: `
   <div>
-  <h2>ユーザーリスト</h2>
+    <h2>ユーザーリスト</h2>
   </div>`,
 };
 
@@ -28,11 +53,11 @@ const UserList = {
   data() {
     return {
       users: [
-        { id: 1, name: 'user1' },
-        { id: 2, name: 'user2' },
-        { id: 3, name: 'user3' },
-        { id: 4, name: 'user4' },
-        { id: 5, name: 'user5' },
+        { id: 1, name: 'user1', address: '001' },
+        { id: 2, name: 'user2', address: '002' },
+        { id: 3, name: 'user3', address: '003' },
+        { id: 4, name: 'user4', address: '004' },
+        { id: 5, name: 'user5', address: '005' },
       ],
       selected_user: {},
     };
@@ -42,10 +67,11 @@ const UserList = {
     <list-title></list-title>
     <ul>
       <li v-for="user in users" :key="user.id" @click='selected_user = user'>
-      {{ user.name }}
+        {{ user.name }}
       </li>
     </ul>
-    <user-detail :user = 'selected_user' :user-name='selected_user.name'></user-detail>
+    <user-detail :user = 'selected_user' 
+    :user-id='selected_user.id' :user-name='selected_user.name' :user-address = 'selected_user.address'></user-detail>
   </div>`,
 };
 
